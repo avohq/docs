0e868f9f9b92b4daf7c768b0ac1aeddbdaca2ca1
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-06-03 17:28:35 +0000

    Support custom base HTML elements for markdown

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from 'react';
 import { AppProps } from 'next/app';
+import { MDXProvider } from '@mdx-js/react';
 
 import '../styles/global.css';
 
 import Layout from '../components/Layout';
+import MDComponents from '../styles/MDComponents';
 
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <MDXProvider components={MDComponents}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </MDXProvider>
 );
 
 export default App;
---
+++ styles/MDComponents.tsx
@@ -0,0 +1,9 @@
+import { FunctionComponent } from 'react';
+
+const H1: FunctionComponent = (props) => <h1 style={{ color: 'tomato' }} {...props} />;
+
+const MDComponents = {
+  h1: H1,
+};
+
+export default MDComponents;
