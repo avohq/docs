0b452ff14e55979d4cba03f6f6eccae0c3d4be72
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-06-18 13:09:13 +0000

    Make SmallSteps collapsible

--- components/SmallSteps.tsx
+++ components/SmallSteps.tsx
@@ -1,38 +1,58 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent, ReactElement, useState } from 'react';
+
+import classNames from 'classnames';
 
 import styles from './SmallSteps.module.scss';
 
 interface StepProps {
   number: number;
   title: string;
+  collapsible: boolean;
 }
 
 export const Step: FunctionComponent<StepProps> = ({
   children,
   number,
   title,
+  collapsible,
 }) => {
+  const [open, setOpen] = useState(true);
+
   return (
     <div className={styles.step}>
-      <div className={styles.stepHeader}>
+      <div className={classNames(styles.stepHeader, !open && styles.collapsed)}>
         <span className={styles.stepNumber}>{number}.</span> {title}
+        {collapsible && (
+          <span className={styles.collapse} onClick={() => setOpen(!open)}>
+            x
+          </span>
+        )}
       </div>
-      <div className={styles.stepContent}>{children}</div>
+      {open && <div className={styles.stepContent}>{children}</div>}
     </div>
   );
 };
 
 interface SmallStepsProps {
   children: ReactElement<StepProps> | ReactElement<StepProps>[];
+  collapsible?: boolean | null;
 }
 
-const SmallSteps: FunctionComponent<SmallStepsProps> = ({ children }) => {
-  return React.Children.map(children, (child, i) => {
-    if (child == null) return null;
-    if (typeof child !== 'object') return child;
-
-    return React.cloneElement(child, { number: i + 1 });
-  });
+const SmallSteps: FunctionComponent<SmallStepsProps> = ({
+  children,
+  collapsible = false,
+}) => {
+  return (
+    <React.Fragment>
+      {React.Children.map(children, (child, i) => {
+        if (child == null) return null;
+        return React.cloneElement<StepProps>(child, {
+          number: i + 1,
+          collapsible,
+        });
+      })}
+    </React.Fragment>
+  );
 };
 
 export default SmallSteps;
