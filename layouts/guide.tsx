a887eff3e1a4fbdd19147a9596b61cbaf82a5996
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-07-07 16:50:22 +0000

    Move StickyBox up a level

--- layouts/guide.tsx
+++ layouts/guide.tsx
@@ -2,6 +2,7 @@ import { FunctionComponent } from 'react';
 import createLayout, { layoutProps } from './layout';
 import readingTime from 'reading-time';
 import innerText from 'react-innertext';
+import StickyBox from 'react-sticky-box';
 
 import styles from './guide.module.scss';
 import generateToc from '../util/generateToc';
@@ -20,12 +21,23 @@ const Guide: FunctionComponent<layoutProps> = ({ frontMatter, children }) => {
       </Head>
       <div className={styles.content}>
         <h1 className={styles.title}>{frontMatter.title}</h1>
-        <p className={styles.abstract}>{frontMatter.abstract}</p>
-        <div>{Math.ceil(time.minutes)} minute read</div>
+        {frontMatter.abstract && (
+          <p className={styles.abstract}>{frontMatter.abstract}</p>
+        )}
+        <div className={styles.minutes}>
+          {Math.ceil(time.minutes)} minute read
+        </div>
+
+        <div className={styles.inlineToc}>
+          <Toc headings={headings} />
+        </div>
+
         {children}
       </div>
       <div className={styles.sidebar}>
-        <Toc headings={headings} />
+        <StickyBox offsetTop={40} offsetBottom={20}>
+          <Toc headings={headings} />
+        </StickyBox>
       </div>
     </div>
   );
