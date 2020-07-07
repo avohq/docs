8575509b1266bd1c59e577bda68e9779afcd95ef
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-07-07 12:16:17 +0000

    Add position to Front Matter errors

--- util/mdxLint/rules/frontMatter.ts
+++ util/mdxLint/rules/frontMatter.ts
@@ -2,6 +2,11 @@ import visit from 'unist-util-visit';
 import { RuleFunction, MDXError } from '../linter';
 import YAML from 'yaml';
 
+const position = {
+  start: { line: 1, column: 1 },
+  end: { line: 1, column: 1 },
+};
+
 const lintYaml: RuleFunction = (node, { filePath }) => {
   let frontMatter: Record<string, unknown> = {};
 
@@ -14,13 +19,15 @@ const lintYaml: RuleFunction = (node, { filePath }) => {
 
   if (frontMatter.layout == null)
     errors.push({
-      message: 'Layout attribute missing from FrontMatter',
+      message: 'Layout attribute missing from front matter',
+      position,
       filePath,
     });
 
   if (frontMatter.title == null)
     errors.push({
-      message: 'Title attribute missing from FrontMatter',
+      message: 'Title attribute missing from front matter',
+      position,
       filePath,
     });
 
