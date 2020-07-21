3314829534effc6d0c6bc7437d7b1e5c69205248
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-07-21 11:52:00 +0000

    Add back link

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useEffect, useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import Navigation from './Navigation';
 import { HamburgerButton } from 'react-hamburger-button';
 
@@ -9,6 +9,7 @@ import Logo from './Logo';
 import Footer from './Footer';
 import Header from './Header';
 import useAvoPath from '../util/useAvoPath';
+import Icon from './Icon';
 
 const Layout: FunctionComponent = ({ children }) => {
   const [navigationOpened, setNavigationOpened] = useState(false);
@@ -28,22 +29,26 @@ const Layout: FunctionComponent = ({ children }) => {
 
   return (
     <div className={styles.grid}>
+      <div className={styles.topBar}>
+        <a className={styles.backToAvo} href="https://avo.app">
+          <Icon name="chevron-left" relativeSize="sm" color="inherit" /> Avo
+          home
+        </a>
+      </div>
       <div className={styles.navHeader}>
-        {/* <Hamburger
-          open={navigationOpened}
-          toggle={() => setNavigationOpened(!navigationOpened)}
-        /> */}
-        <Logo width={140} />
-        <div className={styles.hamburgerButton}>
-          <HamburgerButton
-            open={navigationOpened}
-            onClick={() => setNavigationOpened(!navigationOpened)}
-            width={30}
-            height={20}
-            strokeWidth={2}
-            color="black"
-            animationDuration={0.5}
-          />
+        <div className={styles.logoWrapper}>
+          <Logo width={140} />
+          <div className={styles.hamburgerButton}>
+            <HamburgerButton
+              open={navigationOpened}
+              onClick={() => setNavigationOpened(!navigationOpened)}
+              width={30}
+              height={20}
+              strokeWidth={2}
+              color="black"
+              animationDuration={0.5}
+            />
+          </div>
         </div>
       </div>
       <div className={styles.header}>
