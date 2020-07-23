cecd6645ef88004b289118c981a8a5a1c7ffd9e7
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-07-23 10:52:46 +0000

    Fix lint

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -4,7 +4,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
 import {
   faHome,
-  faTerminal,
   faAward,
   faHeartbeat,
   faLifeRing,
