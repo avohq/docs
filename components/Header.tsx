d636ca68b80b11033e16ad74b2e361e7ebde52bd
Valtýr Örn Kjartansson <valtyr@gmail.com>
2020-08-10 15:22:42 +0000

    Revert "Add DocSearch"

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,11 @@
 import { FunctionComponent } from 'react';
 import SignIn from './SignIn';
-import DocSearch from './DocSearch';
 
 import styles from './Header.module.scss';
 
 const Header: FunctionComponent = () => {
   return (
     <div className={styles.root}>
-      <DocSearch />
       <div className={styles.fill}></div>
       <SignIn />
     </div>
