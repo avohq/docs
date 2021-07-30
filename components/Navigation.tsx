16fb7c8188c8f89ab611c6d724212c16ef8d69ad
Alexey Verein <tpom6oh@gmail.com>
2021-07-30 14:41:21 +0300

    Create Using groups and group properties doc

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -212,6 +212,11 @@ const navigation: navigationItem[] = [
             path: '/data-design/global-namespace',
           },
           {
+            type: 'route',
+            title: 'Using Groups and Group Properties',
+            path: '/data-design/groups',
+          },
+          {
             type: 'section',
             group: 'Guides',
           },
