e67db34182b1b582b519ed6b7f9c4cb502c079cb
Bragi Bergþórsson <bragur@gmail.com>
2022-09-21 14:41:42 +0000

    Fix spelling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,12 +33,6 @@ const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   const path = useAvoPath();
 
   useEffect(() => {
-
-    console.log("Hey");
-    return undefined;
-  })
-
-  useEffect(() => {
     const onCopy = () => {
       const content = window.getSelection()?.toString();
       Avo.contentCopied({ path, content });
