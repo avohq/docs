d7b1292c46dfb955aff472e44c4e4b183a46f540
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-02-09 14:51:54 +0000

    Use Amplitude Destination interface in Avo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AnalyticsBrowser } from '@segment/analytics-next';
 import { AppProps } from 'next/app';
 import { MDXProvider } from '@mdx-js/react';
 import mixpanel from 'mixpanel-browser';
+import * as amplitude from '@amplitude/analytics-browser';
 
 import Avo, { AvoEnv, CustomDestination } from '../Avo';
 
@@ -97,12 +98,67 @@ const mixpanelDestinationInterface: CustomDestination = {
   // Mixpanel does not support revenue tracking out of the box.
 };
 
+const amplitudeDestinationInterface: CustomDestination = {
+  make: (_env, apiKey) => amplitude.init(apiKey),
+
+  identify: (userId) => amplitude.setUserId(userId),
+
+  logEvent: (eventName, eventProperties) =>
+    amplitude.track(eventName, eventProperties),
+
+  setUserProperties: (_userId, userProperties) => {
+    const identify = new amplitude.Identify();
+    Object.entries(userProperties).forEach(([key, value]) =>
+      identify.set(key, value),
+    );
+    amplitude.identify(identify);
+  },
+
+  unidentify: () => amplitude.reset(),
+
+  revenue: (amount, eventProperties) => {
+    const event = new amplitude.Revenue().setPrice(amount);
+    if (eventProperties.quantity) {
+      event.setQuantity(eventProperties.quantity);
+    }
+
+    if (eventProperties.productId) {
+      event.setProductId(eventProperties.productId);
+    }
+
+    if (eventProperties.revenueType) {
+      event.setRevenueType(eventProperties.revenueType);
+    }
+
+    if (Object.keys(eventProperties).length > 0) {
+      event.setEventProperties(eventProperties);
+    }
+    amplitude.revenue(event);
+  },
+
+  // Amplitude supports some group features on the Growth and Enterprise plans
+  setGroupProperties: (groupTypeName, groupId, groupProperties) => {
+    const group = new amplitude.Identify();
+    Object.entries(groupProperties).forEach(([key, value]) =>
+      group.set(key, value),
+    );
+    amplitude.groupIdentify(groupTypeName, groupId, group);
+  },
+
+  addCurrentUserToGroup: (groupTypeName, groupId) =>
+    amplitude.setGroup(groupTypeName, groupId),
+
+  // Amplitude doesn't support logging events with groups
+  // logEventWithGroups: (eventName, eventProperties, groupTypeNamesToGroupIds) => {},
+};
+
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   Avo.initAvo(
     { env: getAvoEnv() },
     { client: 'Docs', version: '2.0' },
     {},
     segmentDestinationInterface,
+    amplitudeDestinationInterface,
     mixpanelDestinationInterface,
   );
 
