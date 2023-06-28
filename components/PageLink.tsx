28d937ef8143d8a9a16eab920cd33824436c5598
Hugi Hlynsson <hugihlynsson@gmail.com>
2023-06-28 11:17:59 +0000

    Update PageLink look

--- components/PageLink.tsx
+++ components/PageLink.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import React from 'react';
 
 import styles from './PageLink.module.scss';
 import Link from 'next/link';
@@ -12,51 +12,50 @@ interface Props {
   href: string;
 }
 
-const PageLink: FunctionComponent<Props> = ({
-  title,
-  description,
-  image,
-  href,
-}) => {
+const PageLink: React.FC<Props> = ({ title, description, image, href }) => {
   let { resolvedTheme } = useTheme();
 
+  let isDark = resolvedTheme === 'dark';
+
   return (
-    <Link scroll={true} href={href}>
-      <div className={styles.root}>
-        {image && (
-          <div className={styles.image}>
-            <Image src={image} alt="" width={80} height={80} />
-          </div>
-        )}
-        <div className={styles.text}>
-          <div
-            className={styles.title}
-            style={resolvedTheme === 'dark' ? { color: 'white' } : {}}
+    <Link
+      scroll={true}
+      href={href}
+      className={styles.root}
+      style={{ borderColor: isDark ? '#434A54' : '#E9E9E9' }}
+    >
+      {image && (
+        <div className={styles.image}>
+          <Image src={image} alt="" width={80} height={80} />
+        </div>
+      )}
+      <div className={styles.text}>
+        <div className={styles.title} style={isDark ? { color: 'white' } : {}}>
+          {title}
+        </div>
+        <div
+          className={styles.description}
+          style={isDark ? { color: '#AAB2BD' } : {}}
+        >
+          {description}
+        </div>
+        <div className={styles.callToAction}>
+          <span
+            style={{
+              textDecoration: 'none',
+              color: isDark ? 'lightGrey' : 'grey',
+            }}
           >
-            {title}
-          </div>
-          <div
-            className={styles.description}
-            style={
-              resolvedTheme === 'dark' ? { color: 'rgb(229, 231, 235)' } : {}
-            }
-          >
-            {description}
-          </div>
-          <div className={styles.callToAction}>
-            <span
-              style={{
-                textDecoration: 'none',
-                color: resolvedTheme === 'dark' ? 'lightGrey' : 'grey',
-              }}
-            >
-              {'Read '}
-            </span>
-          </div>
+            {'Read '}
+          </span>
         </div>
       </div>
     </Link>
   );
+};
+
+export const TwoCol: React.FC<{children: React.ReactNode}> = ({ children }) => {
+  return <div className={styles.twoCol}>{children}</div>;
 };
 
 export default PageLink;
