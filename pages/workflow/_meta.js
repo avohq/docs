754dd36dcb025040e641d812e3c6bded91ceef80
Hugi Hlynsson <hugihlynsson@gmail.com>
2024-10-23 16:07:36 +0000

    Fix order of workflow pages

--- pages/workflow/_meta.js
+++ pages/workflow/_meta.js
@@ -1,3 +1,9 @@
 export default {
-  "overview": "Overview"
+  "overview": "Overview",
+  "plan": "1. Plan analytics update in Avo branch",
+  "review": "2. Review suggestions in Avo",
+  "request-implementation": "3. Request implementation",
+  "implement": "4. Implement analytics events",
+  "validate": "5. Validate implementation",
+  "merge-publish": "6. Merge and Publish"
 };
