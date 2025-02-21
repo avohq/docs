13f36f22350945928560a58a35919066e2e3fd9d
Thora Gudfinnsdottir <thora@avo.sh>
2025-02-21 09:12:11 +0000

    Added docs for pushing items between branches.

--- pages/data-design/branches/_meta.js
+++ pages/data-design/branches/_meta.js
@@ -2,5 +2,6 @@ export default {
   "index": "Quickstart: Branches",
   "approval-workflows": "Approval workflows",
   "code-changes": "Review code changes",
+  "pulling-pushing-items-between-branches": "Pulling/pushing items between branches",
   "workspace-notifications": "Notifications"
 };
