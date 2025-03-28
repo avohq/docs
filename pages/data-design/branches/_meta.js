18087d0813254a1b7a1d6cddbb570694f33a481b
Thora Gudfinnsdottir <thora@avo.sh>
2025-03-28 11:42:25 +0000

    Renamed to cross-branch implementaiton

--- pages/data-design/branches/_meta.js
+++ pages/data-design/branches/_meta.js
@@ -2,6 +2,6 @@ export default {
   "index": "Quickstart: Branches",
   "approval-workflows": "Approval workflows",
   "code-changes": "Review code changes",
-  "pulling-pushing-items-between-branches": "Pulling/pushing items between branches",
+  "cross-branch-data-design": "Cross-branch data design",
   "workspace-notifications": "Notifications"
 };
