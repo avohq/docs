674371fa45b3395de91cee0ba4d1afefe15fdeda
Thora Gudfinnsdottir <thora@avo.sh>
2025-03-28 13:44:16 +0000

    keep the path the same as before.

--- pages/data-design/branches/_meta.js
+++ pages/data-design/branches/_meta.js
@@ -2,6 +2,6 @@ export default {
   "index": "Quickstart: Branches",
   "approval-workflows": "Approval workflows",
   "code-changes": "Review code changes",
-  "cross-branch-data-design": "Cross-branch data design",
+  "pulling-pushing-items-between-branches": "Cross-branch data design",
   "workspace-notifications": "Notifications"
 };
