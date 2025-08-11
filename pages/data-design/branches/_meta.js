b52ff27e20bcd81406308e71a9dff39e58f48a07
Thora Gudfinnsdottir <thora@avo.sh>
2025-08-11 13:55:20 +0000

    Added docs for branch context.

--- pages/data-design/branches/_meta.js
+++ pages/data-design/branches/_meta.js
@@ -1,6 +1,7 @@
 export default {
   "index": "Quickstart: Branches",
   "approval-workflows": "Approval workflows",
+  "branch-changes": "Branch changes",
   "code-changes": "Review code changes",
   "pulling-pushing-items-between-branches": "Cross-branch data design",
   "workspace-notifications": "Notifications"
